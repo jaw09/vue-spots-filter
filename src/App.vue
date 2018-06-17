4022218760919f20fe26f5457799ea082449de85
jiananWang <a0918666589@gmail.com>
2018-06-17 22:42:01 +0800

    git init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <h1>Test Success</h1>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="scss">
+</style>
