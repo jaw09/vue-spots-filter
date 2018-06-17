05e1fa257e08eee25c7ef6d9dd8a5d599a60b7f7
jiananWang <a0918666589@gmail.com>
2018-06-18 03:06:40 +0800

    add Navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <template>
   <div id="app">
+    <app-navbar></app-navbar>
     <h1>Test Success</h1>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import Navbar from './components/Navbar'
 export default {
   name: 'App',
+  components: {
+    AppNavbar: Navbar
+  },
   created () {
     const vm = this
     vm.$store.dispatch('setData')
---
+++ src/components/Navbar.vue
@@ -0,0 +1,44 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-primary">
+    <router-link class="navbar-brand text-white text-bold text-animation" to="/">
+      Kaoshung's Scenic Spots
+    </router-link>
+      <form class="my-2 my-lg-0 w-50">
+        <input class="ml-3" type="search" placeholder="Explore your own activities" aria-label="Search">
+        <span class="separator"> </span>
+      </form>
+</nav>
+</template>
+
+<style lang="scss">
+@import 'bootstrap/scss/_functions.scss';
+@import '../assets/helpers/_variables.scss';
+
+.text-bold {
+  font-weight: bold;
+}
+
+input[type='search'] {
+  width: 100%;
+  padding: 0.5rem 10px;
+  background: none;
+  color: $white;
+  border: none;
+  border-bottom: 1px solid $gray-100;
+  &:focus + .separator {
+    transform: scaleX(1) translateY(-2px);
+    opacity: 1;
+  }
+}
+
+.separator {
+  height: 3px;
+  width: 100%;
+  background: $white;
+  display: block;
+  transform: scaleX(0) translateY(-2px);
+  transform-origin: 50%;
+  opacity: 0;
+  transition: all 0.2s linear;
+}
+</style>
