f0e9723031bc30f6d04b78fc0c83e369ec3332eb
jiananWang <a0918666589@gmail.com>
2018-06-17 23:55:15 +0800

    feat fetch api data store into state

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,22 @@
   <div id="app">
     <h1>Test Success</h1>
     <router-view/>
+    {{data}}
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  computed: {
+    data () {
+      return this.$store.getters.data
+    }
+  },
+  created () {
+    const vm = this
+    vm.$store.dispatch('setData')
+  }
 }
 </script>
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,42 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
+const state = {
+  data: {}
+}
 
+const mutations = {
+  'SET_DATA' (state, fetchData) {
+    state.data = fetchData
+  }
+}
+
+const actions = {
+  setData ({
+    commit
+  }) {
+    axios.get('https://data.kcg.gov.tw/api/action/datastore_search?resource_id=92290ee5-6e61-456f-80c0-249eae2fcc97')
+      .then(res => {
+        const fetchData = res.data.result.records
+        commit('SET_DATA', fetchData)
+      }).catch(err => {
+        console.log(err)
+      })
+  }
+}
+
+const getters = {
+  data (state) {
+    return state.data
+  }
+}
+
+export default new Vuex.Store({
+  state,
+  mutations,
+  actions,
+  getters
 })
